775154867cceb7bd660d7132708ecf3264de8714
EC2 Default User <ec2-user@ip-172-31-90-174.ec2.internal>
2025-04-27 23:27:54 +0000

    second commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+importReact from'react';
+ import logofrom'./logo.svg';
+ import './App.css';
+ functionApp(){
+ return(
+ <divclassName="App">
+ <headerclassName="App-header">
+ <imgsrc={logo}className="App-logo"alt="logo"/>
+ <h1>HellofromV2</h1>
+ </header>
+ </div>
+ );
+ }
+ exportdefaultApp;
